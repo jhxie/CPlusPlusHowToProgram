8ef44f22d4e430647a073ab48d53388606ab1ce5
Jiahui Xie <jiahui.xie@outlook.com>
2018-01-02 17:47:30 +0800

    Implement 'CMake' module 'FindGraphViz'
    Build System
    
    - Let 'CMake' not to build 'GraphViz' targets in the 'doc' sub-directory if
      'GraphViz' is not installed; the changes are made in 'doc/CMakeLists.txt'
    
    - Use 'FindEGL' 'CMake' module as a base to implement 'FindGraphViz' in
      'cmake/modules/FindGraphViz.cmake'

---
+++ cmake/modules/FindGraphViz.cmake
@@ -0,0 +1,32 @@
+# Try to find 'GraphViz'.
+#
+# This will define the following variables:
+#
+# GRAPHVIZ_FOUND
+# True if all the layout engines ("dot", "neato", "twopi", "circo", "fdp") are
+# available.
+
+set(_graphviz_found TRUE)
+foreach(layout_engine "dot;neato;twopi;circo;fdp")
+    find_program(GRAPHVIZ NAMES ${layout_engine})
+    if(${GRAPHVIZ} STREQUAL "GRAPHVIZ-NOTFOUND")
+        set(_graphviz_found FALSE)
+        break()
+    endif()
+endforeach()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GraphViz
+    FOUND_VAR
+        GraphViz_FOUND
+    REQUIRED_VARS
+       _graphviz_found
+)
+
+unset(_graphviz_found)
+
+include(FeatureSummary)
+set_package_properties(GraphViz PROPERTIES
+    URL "https://graphviz.org/"
+    DESCRIPTION "Graphviz is open source graph visualization software."
+)
